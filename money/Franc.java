4cd5ea8b46d5b078d8b02d646fad7b42e9386c4b
karolWazny <karoljanwazny@gmail.com>
2021-07-02 18:10:31 +0200

    dodanie klasy money

---
+++ money/Franc.java
@@ -0,0 +1,16 @@
+package money;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount){
+        this.amount = amount;
+    }
+    public Franc times(int multiplier){
+        return new Franc(this.amount * multiplier);
+    }
+    public boolean equals(Object object){
+        Franc Franc = (Franc) object;
+        return amount == Franc.amount;
+    }
+}
--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -5,16 +5,22 @@ import org.junit.Test;
 
 public class MoneyTest extends TestCase {
     @Test
-    public void testMultiplication(){
+    public void testDollarMultiplication(){
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
     @Test
-    public void testEquality(){
+    public void testDollarEquality(){
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(6).equals(new Dollar(5)));
+    }
+
+    @Test
+    public void testFrancMultiplication(){
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
