4cb30e600f27aca64ea7cedf23876f0612678f3a
karolWazny <karoljanwazny@gmail.com>
2021-09-06 20:53:12 +0200

    rozdział 14 part 2

--- money/Bank.java
+++ money/Bank.java
@@ -1,10 +1,41 @@
 package money;
 
+import java.util.Hashtable;
+
 public class Bank {
+    private Hashtable rates = new Hashtable();
+
     Money reduce(Expression source, String to){
         return source.reduce(this, to);
     }
 
-    public void addRate(String chf, String usd, int i) {
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), Integer.valueOf(rate));
+    }
+
+    public int rate(String from, String to){
+        if(from.equals(to))
+            return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    private class Pair {
+        private String from;
+        private String to;
+
+        public Pair(String from, String to){
+            this.from = from;
+            this.to = to;
+        }
+
+        public boolean equals(Object object){
+            Pair pair = (Pair) object;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        public int hashCode(){
+            return 0;
+        }
     }
 }
